0dd593ce8edfceea5123b4cfd584a9fca0a94599
thoriqwildan <wildanthoriq14@gmail.com>
2024-07-19 16:20:47 +0700

    upload to vercel#2

--- harjaweb/settings.py
+++ harjaweb/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = ['static/']
+STATICFILES_DIRS = [
+    BASE_DIR /'static/',
+    ]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
